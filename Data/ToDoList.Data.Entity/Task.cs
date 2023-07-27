9431a0c5a52732c3a60f823729828c6cc105aa9d
Alix Allegre <alixallegre31@gmail.com>
2023-07-27 11:47:12 +0200

    first commit

---
+++ Data/ToDoList.Data.Entity/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Data.Entity
+{
+    public class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TaskId { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DueDate { get; set;}
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+        public int StatusId { get; set; }
+        public virtual Status Status { get; set; }
+    }
+}
